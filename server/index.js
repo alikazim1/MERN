579294db612a28aa21765baeab4e1d0df588b79f
alikazim1 <115345833+alikazim1@users.noreply.github.com>
2025-04-16 20:24:19 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://mern-fr-76zz.onrender.com"], // Match the deployed frontend
+    origin: ["https://mern-fnt.onrender.com"], // Match the deployed frontend
     methods: ["POST", "GET"],
     credentials: true,
   })
